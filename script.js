ddde5ec58b614338734aa9cbdd3a020e4f6af00f
Yana <38989612+IanaOrlova@users.noreply.github.com>
2019-03-04 22:41:24 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+
+    $('.slider1').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+
+    });
+
+    $('.slider2').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+
+    });
+
+})
